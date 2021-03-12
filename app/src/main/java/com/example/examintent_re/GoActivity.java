087e0e052e3e3b65e09338cec7b7123d55151321
jinchaeeun <kbg05032@naver.com>
2021-03-12 13:01:48 +0900

    ArrayList 문자열 넘기기 Intent

--- app/src/main/java/com/example/examintent_re/GoActivity.java
+++ app/src/main/java/com/example/examintent_re/GoActivity.java
@@ -4,15 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class GoActivity extends AppCompatActivity {
+    // Member Variable -----------------------------------------------------------
+    private final boolean D = true;
+    private final String TAG = "GoActivity";
+    private TextView                msgTXT;
 
+    // Member Override -----------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(D) Log.i(TAG, "onCreate()");
         setContentView(R.layout.layout_go);
+        init();
+    }
 
-        Intent intent= getIntent();
+    // Member Method - Custom ----------------------------------------------------
+    private void init(){
+        // 현재 Activity 실행한 Intent 객체 가져오기
+        Intent rxIntent = this.getIntent();
+        //String rxData = rxIntent.getStringExtra(AppConstant.KEY_NAME);
+        ArrayList<String> rxData = (ArrayList<String>) rxIntent.getSerializableExtra(AppConstant.KEY_NAMES); //getSerializableExtra객체로 준 걸 객체로 그대로 받아오겠다.
+        // View 객체 가져오기
+        msgTXT = findViewById(R.id.msgTXT);
 
+        //rxData가 없으면 null이 들어오니 확인하기
+        //if(rxData != null) msgTXT.setText(rxData);
+
+        if(rxData != null){
+            String data = "";
+            for(int i=0; i<rxData.size(); i++){
+                data += rxData.get(i) + "\n";
+            }
+            msgTXT.setText(data);
+        }
     }
 }
--- app/src/main/java/com/example/examintent_re/MainActivity.java
+++ app/src/main/java/com/example/examintent_re/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 
 import com.example.examintent_re.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     // Member Variable
     private final boolean D = true;
@@ -51,12 +53,35 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Log.i(TAG, "onCreate() OK");
-
+        Log.i(TAG, "onItemClick() OK");
+        Intent intent;
         switch (position) {
-            case 0:
-                Intent intent = new Intent(MainActivity.this, GoActivity.class);
+            case AppConstant.ITEM_GO:
+                //GoActivity 전환
+                intent = new Intent(MainActivity.this, GoActivity.class);
                 startActivity(intent);
+                break;
+            case AppConstant.ITEM_GO_WITH_DATA:
+                // Primitive Data와 함께 전달
+                intent = new Intent(MainActivity.this, GoActivity.class);
+                intent.putExtra(AppConstant.KEY_NAME, "KIM"); //Key값 설정하기 위해 AppConstant.java에서 문자열 선언
+                intent.putExtra(AppConstant.KEY_PHONE,"010-1111-2222");
+
+                // 데이터 대량으로 보내기
+                // Primitive Data List 함께 전달
+                ArrayList<String>   names = new ArrayList<String>();
+                names.add("Hong");
+                names.add("Choi");
+                names.add("Park");
+                names.add("Kwon");
+
+
+                intent.putExtra(AppConstant.KEY_NAMES, names);      //가져오는 것 GoActivity
+                startActivity(intent);
+                break;
+            case AppConstant.ITEM_GO_WITH_OBJECT:
+                break;
+            case AppConstant.ITEM_GO_BACK:
                 break;
         }
 
